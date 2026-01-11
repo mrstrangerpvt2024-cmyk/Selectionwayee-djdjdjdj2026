e9719c204ad8973a8f56607d64776b1be95d7574
mrstrangerpvt2024-cmyk <mrstrangerpvt2024@gmail.com>
2026-01-11 21:29:43 +0530

    Update selection.py

--- selection.py
+++ selection.py
@@ -4,7 +4,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
 
 # Bot Configuration
-BOT_TOKEN = "8424175338:AAFMsg2-1IPVwGhZB9-oGWYq0O4CofTu28k"
+BOT_TOKEN = "7254519583:AAHCuy42E2GbwNFzfUF1Gjt5oBAoOy_i7Kw"
 
 # Enable logging
 logging.basicConfig(
@@ -536,3 +536,4 @@ def main():
 if __name__ == '__main__':
 
     main()
+
